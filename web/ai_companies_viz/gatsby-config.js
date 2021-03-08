a5c5c70b78edb2532acb78c94e7db8c3509be438
Jennifer Melot <jtmelot@gmail.com>
2021-03-08 10:29:08 -0500

    Add favicon

--- web/ai_companies_viz/gatsby-config.js
+++ web/ai_companies_viz/gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: "ai_companies_viz",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        start_url: "/",
+        icon: "src/images/favicon.ico", // This path is relative to the root of the site.
+      },
+    }
+  ],
 };
