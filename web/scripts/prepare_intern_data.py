594ce3b893129a66489549d8817e6004d61e6f13
Jennifer Melot <jtmelot@gmail.com>
2021-03-15 13:46:02 -0400

    Finalize method for generating intern description annotation data

--- web/scripts/prepare_intern_data.py
+++ web/scripts/prepare_intern_data.py
@@ -1,21 +1,28 @@
 import argparse
 import csv
 import json
+import urllib.parse
 
 def get_intern_data(input_fi, output_fi):
-    out = csv.DictWriter(open(output_fi, mode="w"),
-            fieldnames=["company_name", "aliases", "company_link", "wikipedia_description", "wikipedia_link",
-                        "company_description", "crunchbase_description"])
-    out.writeheader()
+    rows = []
     with open(input_fi) as f:
         for line in f:
             js = json.loads(line)
-            out.writerow({
+            has_null_description = ("short_description" not in js) or (not js["short_description"])
+            encoded_wiki_search = urllib.parse.urlencode({"search": js["name"]})
+            rows.append({
                 "company_name": js["name"],
-                "aliases": "; ".join([a["alias"] for a in js["aliases"]]),
-                "company_link": js["website"],
-                "crunchbase_description": "" if "short_description" not in js else js["short_description"]
+                "wikipedia_search": f"https://en.wikipedia.org/wiki/Special:Search?"+encoded_wiki_search,
+                "company_homepage": js["website"],
+                "crunchbase_description": "" if has_null_description else js["short_description"]
             })
+    with open(output_fi, mode="w") as f:
+        out = csv.DictWriter(f, fieldnames=["company_name", "crunchbase_description", "wikipedia_search",
+                                            "wikipedia_description", "wikipedia_description_link", "company_homepage",
+                                            "company_description", "company_description_link"])
+        out.writeheader()
+        for row in sorted(rows, key=lambda r: len(r["crunchbase_description"])):
+            out.writerow(row)
 
 
 if __name__ == "__main__":
