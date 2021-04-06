c7bbbe28f8469beda100d7215607724bdba57144
Jennifer Melot <jtmelot@gmail.com>
2021-04-06 16:17:08 -0400

    Toggle description visibility

--- web/ai_companies_viz/src/pages/index.js
+++ web/ai_companies_viz/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import Button from "@material-ui/core/Button";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Link from "@material-ui/core/Link";
 import PropTypes from "prop-types";
@@ -55,50 +56,62 @@ const IndexPage = () => {
   // thank you https://stackoverflow.com/a/63066975
   const isSSR = typeof window === "undefined";
 
+  const [showDesc, setShowDesc] = React.useState(false);
+  const toggleDesc = () => {
+    setShowDesc(!showDesc);
+  };
+
   const [selectedTab, setSelectedTab] = React.useState("overview");
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
 
   return (
-    <main>
-      <div style={{paddingBottom: "50px"}}>
+    <main style={{minWidth: "1220px"}}>
+      <div id="toolbar" style={{"margin": "20px"}}>
+        <a href={"https://cset.georgetown.edu"} target="_blank" rel="noreferrer" title="Link to CSET website, cset.georgetown.edu">
+          <img src={cset_logo} style={{"width": "300px"}} alt="CSET Logo"/>
+        </a>
+        <Button variant="contained"
+                color="primary"
+                style={{"float": "right"}}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdMNP7fg3_HkIdKh_IZBksm6vZCbzb1cRZS-sdeLL5i3yxi_g/viewform"
+                target="_blank">
+          Questions and Submissions
+        </Button>
+      </div>
+      <div>
         <div style={{margin: "20px 20px 30px 20px"}}>
-          <div style={{width: "80%", marginLeft: "1%", display: "inline-block"}}>
-            <h2>🥕 Welcome to CARAT, CSET's tracker for private-sector AI activity.</h2>
+          <div style={{marginLeft: "1%", display: "inline-block"}}>
+            <h2><span role={"img"} aria-label={"carat logo"}>🥕</span> Welcome to CARAT, CSET's tracker for private-sector AI activity.</h2>
             <h3 style={{fontWeight: "normal"}}>
               CARAT collects data related to companies' AI research, development, and production activities
-              in order to inform analysis of the global AI sector.
+              in order to inform analysis of the global AI sector. <Link onClick={toggleDesc} style={{cursor: "pointer"}}>{showDesc ? "Hide details..." : "Show details..."}</Link>
             </h3>
           </div>
-          <div style={{width: "19%", display: "inline-block", textAlign: "right", verticalAlign: "top"}}>
-            <a href={"https://cset.georgetown.edu"} target="_blank" rel="noreferrer" title="Link to CSET website, cset.georgetown.edu">
-                <img src={cset_logo} style={{"width": "100%"}} alt="CSET Logo"/>
-            </a>
+        </div>
+        <div style={{"display": showDesc ? "block" : "none", paddingBottom: "50px"}}>
+          <Tabs value={selectedTab} onChange={handleTabChange} orientation={"vertical"}
+                style={{borderRight: "1px solid grey", width: "15%", display: "inline-block", minWidth: "160px"}}>
+            <Tab
+              value="overview"
+              label="Overview"
+              indicatorColor="primary"
+              textColor="primary"
+              {...a11yProps('overview')}
+            />
+            <Tab value="data_sources" label="Data Sources" {...a11yProps('data_sources')} />
+            <Tab value="methodology" label="Methodology" {...a11yProps('methodology')} />
+            <Tab value="faq" label="FAQ" {...a11yProps('faq')} />
+            <Tab value="acknowledgments" label="Acknowledgements" {...a11yProps('acknowledgments')} />
+          </Tabs>
+          <div style={{verticalAlign: "top", display: "inline-block", width: "84.5%"}}>
+            <TabPanel value={selectedTab} index="overview"/>
+            <TabPanel value={selectedTab} index="data_sources"/>
+            <TabPanel value={selectedTab} index="methodology"/>
+            <TabPanel value={selectedTab} index="faq"/>
+            <TabPanel value={selectedTab} index="acknowledgments"/>
           </div>
-        </div>
-        <div>
-        <Tabs value={selectedTab} onChange={handleTabChange} orientation={"vertical"}
-              style={{borderRight: "1px solid grey", width: "15%", display: "inline-block", minWidth: "160px"}}>
-          <Tab
-            value="overview"
-            label="Overview"
-            indicatorColor="primary"
-            textColor="primary"
-            {...a11yProps('overview')}
-          />
-          <Tab value="data_sources" label="Data Sources" {...a11yProps('data_sources')} />
-          <Tab value="methodology" label="Methodology" {...a11yProps('methodology')} />
-          <Tab value="faq" label="FAQ" {...a11yProps('faq')} />
-          <Tab value="acknowledgments" label="Acknowledgements" {...a11yProps('acknowledgments')} />
-        </Tabs>
-        <div style={{verticalAlign: "top", display: "inline-block", width: "84.5%"}}>
-          <TabPanel value={selectedTab} index="overview"/>
-          <TabPanel value={selectedTab} index="data_sources"/>
-          <TabPanel value={selectedTab} index="methodology"/>
-          <TabPanel value={selectedTab} index="faq"/>
-          <TabPanel value={selectedTab} index="acknowledgments"/>
-        </div>
         </div>
       </div>
       <div style={{padding: "10px 50px", backgroundColor: "#FFFFFF"}} id="table-container">
