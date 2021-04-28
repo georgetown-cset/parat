b5fbe5e996723742362277e806634f33397963ad
Jennifer Melot <jtmelot@gmail.com>
2021-04-28 12:15:10 -0400

    Address @rggelles's code review comments

--- web/tests/test_mk_tab_text.py
+++ web/tests/test_mk_tab_text.py
@@ -6,9 +6,12 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../scripts/')
 
 from web.scripts.mk_tab_text import clean
+from web.scripts.retrieve_data import LINK_CSS
 
 
 class TestMkTabText(unittest.TestCase):
+    maxDiff = None
+
     def test_clean(self):
         input_md = """
 ## Here's a header
@@ -30,8 +33,7 @@ With some other text after it and a [link](http://google.com)!
             {'__html': "<ul><li style='margin:10px 0px'>here's</li>\n"
                     "<li style='margin:10px 0px'>a</li>\n"
                     "<li style='margin:10px 0px'>list</li></ul>"},
-            {'__html': "With some other text after it and a <a class='MuiTypography-root MuiLink-root "
-                       "MuiLink-underlineHover MuiTypography-colorPrimary' href='http://google.com' "
+            {'__html': f"With some other text after it and a <a class={LINK_CSS} href='http://google.com' "
                        "target='_blank' rel='noreferrer'>link</a>!"}
         ]
         self.assertEqual(clean(input_md), expected_output)
