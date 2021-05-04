f00eb4259267e29a8970a2e32ced59d66a399b9a
Jennifer Melot <jtmelot@gmail.com>
2021-05-04 12:07:25 -0400

    Add a filtered table header test

--- web/parat/src/pages/__tests__/table_header.js
+++ web/parat/src/pages/__tests__/table_header.js
@@ -2,8 +2,9 @@ import React from "react"
 import {render} from "@testing-library/react"
 import CompanyTableHead from "../table_header"
 
-test("Table Header defaults", () => {
-  const maxSliderValue = 100;
+const maxSliderValue = 100;
+
+test("Table header defaults", () => {
   const table = document.createElement("table");
   const header = render(
     <CompanyTableHead
@@ -28,6 +29,38 @@ test("Table Header defaults", () => {
         "name": null,
         "country": null,
         "continent": null,
+        "stage": null
+      }}
+    />, {container: document.body.appendChild(table)}
+  );
+  expect(header).toMatchSnapshot();
+});
+
+test("Table header with filters", () => {
+    const table = document.createElement("table");
+  const header = render(
+    <CompanyTableHead
+      order={{
+        "ai_pubs": -1,
+        "ai_pubs_in_top_conferences": -1,
+        "ai_patents": -1
+      }}
+      onRequestSort={() => {}}
+      onFilterRows={() => {}}
+      filterValues={{
+        "ai_pubs": [0, maxSliderValue],
+        "ai_pubs_in_top_conferences": [0, maxSliderValue],
+        "ai_patents": [0, maxSliderValue],
+        "name": ["Microsoft"],
+        "country": ["United States", "China"],
+        "continent": [],
+        "stage": ["Mature", "Startup"]
+      }}
+      maxSliderValue={maxSliderValue}
+      filteredFilters={{
+        "name": ["Microsoft", "Huawei", "Hugging Face", "nullmax"],
+        "country": null,
+        "continent": ["North America", "Asia"],
         "stage": null
       }}
     />, {container: document.body.appendChild(table)}
