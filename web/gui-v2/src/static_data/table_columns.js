c94e929adcebb47532d6345802896084fca3695a
Brian Love <bl711@georgetown.edu>
2023-10-05 16:06:30 -0400

    Simpify column definitions and increase minimum column width
    Simplify the column definitions for slider columns to reduce necessary
    boilerplate.  Set a minimum width for slider columns (and ensured that
    it is actually applied) to reduce crowding in some of the smaller
    columns (like AI/TT1 jobs).

--- web/gui-v2/src/static_data/table_columns.js
+++ web/gui-v2/src/static_data/table_columns.js
@@ -12,6 +12,7 @@ const styles = {
     }
   `,
   sliderColumn: css`
+    min-width: 100px;
     width: 120px;
 
     .MuiButtonBase-root {
@@ -20,29 +21,26 @@ const styles = {
   `,
 };
 
-/** Default values for slider columns */
-const SLIDER_PRESETS = {
-  css: styles.slider,
-  initialCol: false,
-  sortable: true,
-  type: 'slider',
-};
-
 /**
- * Helper function to define the `extract` and `format` functions in a consistent
- * way across all columns.
+ * Helper function to define the `extract` and `format` functions of slider
+ * fields in a consistent way across all columns.
  *
  * @param {string} dataKey
  * @param {string} dataSubkey
  * @returns {{
+ *  css: SerializedStyles,
  *  dataKey: string,
  *  dataSubkey: string,
  *  extract: (val: any, row: object) => any,
  *  format: (val: any, row: object) => ReactNode,
+ *  initialCol: boolean,
+ *  sortable: boolean,
+ *  type: 'dropdown'|'slider',
  * }}
  */
-const generateDataFns = (dataKey, dataSubkey) => {
+const generateSliderColDef = (dataKey, dataSubkey) => {
   return {
+    css: styles.sliderColumn,
     dataKey,
     dataSubkey,
     extract: (_val, row) => {
@@ -50,6 +48,9 @@ const generateDataFns = (dataKey, dataSubkey) => {
       return res === null ? 0 : res;
     },
     format: (_val, row) => <CellStat data={row[dataKey][dataSubkey]} />,
+    initialCol: false,
+    sortable: true,
+    type: 'slider',
   }
 };
 
@@ -70,180 +71,151 @@ export default [
   {
     title: "All publications",
     key: "all_pubs",
-    ...generateDataFns("articles", "all_publications"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "all_publications"),
   },
   {
     title: "Citation counts",
     key: "citations",
-    ...generateDataFns("articles", "citation_counts"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "citation_counts"),
   },
   {
     title: "AI publications",
     key: "ai_pubs",
-    ...generateDataFns("articles", "ai_publications"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "ai_publications"),
     initialCol: true,
   },
   {
     title: "AI publications in top conferences",
     key: "ai_pubs_top_conf",
-    ...generateDataFns("articles", "ai_pubs_top_conf"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "ai_pubs_top_conf"),
   },
   {
     title: "AI patents",
     key: "ai_patents",
-    ...generateDataFns("patents", "ai_patents"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "ai_patents"),
     initialCol: true,
   },
   {
     title: "CV publications",
     key: "cv_pubs",
-    ...generateDataFns("articles", "cv_pubs"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "cv_pubs"),
   },
   {
     title: "NLP publications",
     key: "nlp_pubs",
-    ...generateDataFns("articles", "nlp_pubs"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "nlp_pubs"),
   },
   {
     title: "Robotics publications",
     key: "ro_pubs",
-    ...generateDataFns("articles", "robotics_pubs"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("articles", "robotics_pubs"),
   },
 
   {
     title: "Agricultural patents",
     key: "agri_patents",
-    ...generateDataFns("patents", "Agricultural"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Agricultural"),
   },
   {
     title: "Banking and finance patents",
     key: "finance_patents",
-    ...generateDataFns("patents", "Banking_and_Finance"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Banking_and_Finance"),
   },
   {
     title: "Business patents",
     key: "business_patents",
-    ...generateDataFns("patents", "Business"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Business"),
   },
   {
     title: "Computing in government patents",
     key: "comp_in_gov_patents",
-    ...generateDataFns("patents", "Computing_in_Government"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Computing_in_Government"),
   },
   {
     title: "Document management and publishing patents",
     key: "doc_mgt_patents",
-    ...generateDataFns("patents", "Document_Mgt_and_Publishing"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Document_Mgt_and_Publishing"),
   },
   {
     title: "Education patents",
     key: "edu_patents",
-    ...generateDataFns("patents", "Education"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Education"),
   },
   {
     title: "Energy patents",
     key: "energy_mgt_patents",
-    ...generateDataFns("patents", "Energy_Management"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Energy_Management"),
   },
   {
     title: "Entertainment patents",
     key: "entertain_patents",
-    ...generateDataFns("patents", "Entertainment"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Entertainment"),
   },
   {
     title: "Industrial and manufacturing patents",
     key: "industry_patents",
-    ...generateDataFns("patents", "Industrial_and_Manufacturing"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Industrial_and_Manufacturing"),
   },
   {
     title: "Life sciences patents",
     key: "life_patents",
-    ...generateDataFns("patents", "Life_Sciences"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Life_Sciences"),
   },
   {
     title: "Military patents",
     key: "mil_patents",
-    ...generateDataFns("patents", "Military"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Military"),
   },
   {
     title: "Nanotechnology patents",
     key: "nano_patents",
-    ...generateDataFns("patents", "Nanotechnology"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Nanotechnology"),
   },
   {
     title: "Networks patents",
     key: "network_patents",
-    ...generateDataFns("patents", "Networks__eg_social_IOT_etc"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Networks__eg_social_IOT_etc"),
   },
   {
     title: "Personal devices and computing patents",
     key: "personal_comp_patents",
-    ...generateDataFns("patents", "Personal_Devices_and_Computing"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Personal_Devices_and_Computing"),
   },
   {
     title: "Physical sciences and engineering patents",
     key: "phys_sci_patents",
-    ...generateDataFns("patents", "Physical_Sciences_and_Engineering"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Physical_Sciences_and_Engineering"),
   },
   {
     title: "Security patents",
     key: "security_patents",
-    ...generateDataFns("patents", "Security__eg_cybersecurity"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Security__eg_cybersecurity"),
   },
   {
     title: "Semiconductor patents",
     key: "semiconductor_patents",
-    ...generateDataFns("patents", "Semiconductors"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Semiconductors"),
   },
   {
     title: "Telecommunications patents",
     key: "telecom_patents",
-    ...generateDataFns("patents", "Telecommunications"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Telecommunications"),
   },
   {
     title: "Transportation patents",
     key: "transport_patents",
-    ...generateDataFns("patents", "Transportation"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("patents", "Transportation"),
   },
 
   {
     title: "AI jobs",
     key: "ai_jobs",
-    ...generateDataFns("other_metrics", "ai_jobs"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("other_metrics", "ai_jobs"),
     initialCol: true,
   },
   {
     title: "Tech Tier 1 jobs",
     key: "tt1_jobs",
-    ...generateDataFns("other_metrics", "tt1_jobs"),
-    ...SLIDER_PRESETS,
+    ...generateSliderColDef("other_metrics", "tt1_jobs"),
   },
 ];
