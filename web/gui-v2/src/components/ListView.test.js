bfa95423bdf2d8d547a23383d41bfeafa798279c
Brian Love <bl711@georgetown.edu>
2023-10-20 10:29:53 -0400

    Use callback instead of reusing saved element
    Re-query for the dialog element instead of reusing the prior element as
    suggested in https://github.com/testing-library/dom-testing-library/issues/1204#issuecomment-1581335409

--- web/gui-v2/src/components/ListView.test.js
+++ web/gui-v2/src/components/ListView.test.js
@@ -54,7 +54,7 @@ describe("ListView", () => {
       await user.click(removedCheckbox);
       expect(removedCheckbox.checked).toEqual(false);
       await user.click(getByRole(dialog, 'button', { name: 'Apply' }));
-      await waitForElementToBeRemoved(dialog);
+      await waitForElementToBeRemoved(() => screen.getByRole('dialog'));
       expect(screen.queryByRole('heading', { name: 'Add/remove columns'})).not.toBeInTheDocument();
 
       // Verify that the changes took effect
