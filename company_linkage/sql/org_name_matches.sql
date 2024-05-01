50e8e6045cb7ce6bc4603a2741ebfdb5e8196ce6
Jennifer Melot <jtmelot@gmail.com>
2024-05-01 14:47:23 -0400

    Check regex individually

--- company_linkage/sql/org_name_matches.sql
+++ company_linkage/sql/org_name_matches.sql
@@ -1,8 +1,10 @@
 -- WARNING: As of 2024-04-09, staging_ai_companies_visualization.org_name_cross_regex is > 35 B rows/5 TB
-SELECT
+SELECT DISTINCT
   CSET_id,
   org_name
 FROM
   staging_ai_companies_visualization.org_name_cross_regex
+LEFT JOIN
+  UNNEST(regex) as r
 WHERE
-  REGEXP_CONTAINS(org_name, ARRAY_TO_STRING(regex, "|"))
+  REGEXP_CONTAINS(org_name, r)
