212584d88b99e8ebb3a68c0a915db3bbbcb60916
Jennifer Melot <jtmelot@gmail.com>
2024-05-02 11:26:13 -0400

    Remove comment, move to comment on pr

--- company_linkage/sql/ai_company_papers.sql
+++ company_linkage/sql/ai_company_papers.sql
@@ -7,9 +7,6 @@ WITH unnested_rors AS (
   CROSS JOIN UNNEST(ror_id) as r
 )
 
--- TODO: this query returns 86,219 results, as opposed to the 130,154 we have from the previous method. One difference
--- is that we are no longer including orgs with ror ids in the regex matching. I need Rebecca to review and
--- determine whether we should also do regex match on orgs with ror ids.
 SELECT DISTINCT
   CSET_id,
   merged_id,
