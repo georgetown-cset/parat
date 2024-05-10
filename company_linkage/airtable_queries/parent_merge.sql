f01854ed57abd50dca6313481972bd92e2486c52
Jennifer Melot <jtmelot@gmail.com>
2024-05-10 15:04:31 -0400

    Remove obsolete airtable configuration

--- company_linkage/airtable_queries/parent_merge.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  CSET_id,
-  name,
-  IF(parent_acquisition IS TRUE, TRUE, FALSE) as parent_acquisition,
-  parent_name,
-  parent_id,
-  IF(TRIM(lower(comment)) != "checked", comment, NULL) as comment
-FROM
-  {{ params.staging_dataset }}.{{ params.staging_table_name }}
