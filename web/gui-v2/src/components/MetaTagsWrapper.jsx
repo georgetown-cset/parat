16332827359af0da555cd8c5bce1c07a11bc5f34
Brian Love <bl711@georgetown.edu>
2024-06-25 18:12:37 -0400

    Update OpenGraph description

--- web/gui-v2/src/components/MetaTagsWrapper.jsx
+++ web/gui-v2/src/components/MetaTagsWrapper.jsx
@@ -6,7 +6,7 @@ import socialCard from '@eto/social-cards/dist/tools/parat.png';
 const MetaTagsWrapper = ({
   title = "PARAT \u2013 Emerging Technology Observatory",
   subtitle = undefined,
-  description = "ETO's tool to map high-level trends in global emerging technology research."
+  description = "ETO's hub for data on private-sector companies and their AI activities."
 }) => {
   const fullTitle = subtitle ? `${subtitle} \u2013 ${title}` : title;
 
